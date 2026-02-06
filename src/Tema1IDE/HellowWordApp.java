98cc20d2b60a12e897eb843f2a972722e6766b00
alexissytati <sierramonterito77@gmail.com>
2026-02-06 11:04:44 -0600

    inicio de proyecto ITESS-TICS-POO

---
+++ src/Tema1IDE/HellowWordApp.java
@@ -0,0 +1,19 @@
+/*
+* 20026 ITESS-TICs
+*Periodo:Enero-Junio
+*Asignatura:Programacion Orientada a Objetos
+*Docente: Alexis Garcia Montero
+*Descripcion: Tema 1. Entorno de desarrollo
+*                     Prueba del entorno de desarrollo y github
+*Programador: Alexis Garcia Montero
+*Fecha:06/02/2026
+ */
+package Tema1IDE;
+
+public class HellowWordApp {
+    public static void main(String[] args) {
+        HellowWorld hello= new HellowWorld();
+        
+        hello.sayHellow("Alaxis Garcia Montero");
+    }
+}
---
+++ src/Tema1IDE/HellowWorld.java
@@ -0,0 +1,19 @@
+/*
+* 20026 ITESS-TICs
+*Periodo:Enero-Junio
+*Asignatura:Programacion Orientada a Objetos
+*Docente: Alexis Garcia Montero
+*Descripcion: Tema 1. Entorno de desarrollo
+*                     Prueba del entorno de desarrollo y github
+*Programador: Alexis Garcia Montero
+*Fecha:06/02/2026
+ */
+package Tema1IDE;
+
+public class HellowWorld {
+   
+    public void sayHellow(String nombre){
+        System.out.println("Hellow, "+nombre);
+    }
+         
+}
