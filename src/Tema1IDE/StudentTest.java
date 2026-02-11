753ef57fb84a350cf00c148bc4a0ebc164a280c7
alexissytati <sierramonterito77@gmail.com>
2026-02-11 13:07:53 -0600

    se creo la clase Student

---
+++ src/Tema1IDE/StudentTest.java
@@ -0,0 +1,22 @@
+/*
+ * 2026 ITESS-TICs
+*Periodo:Enero-Junio
+*Asignatura:Programacion Orientada a Objetos
+*Docente: Alexis Garcia Montero
+*Descripcion: Tema 1.- Entorno de desarrollo
+                  Proceso de desarrollo de aplicaciones.
+                     
+*                                    
+*Programador: Alexis Garcia Montero
+*Fecha:10/02/2026
+ */
+package Tema1IDE;
+
+public class StudentTest {
+    
+        public static void main(String[] args) {
+           //Prueba del metodo
+            Student student1 = new Student();
+             System.out.println(student1.toString());
+    }
+}
